17e0a30c40187f45b45c98797a974be340a855dd
Walter De Oliveira Pinto Coelho <1230700@sga.pucminas.br>
2020-03-12 20:31:19 -0300

    Adição e Subtração + testes

--- calculadora_devops/src/com/puc/devops/Main.java
+++ calculadora_devops/src/com/puc/devops/Main.java
@@ -5,4 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("");
     }
+
+    private double Subtrai(double a, double b)
+    {
+        return calculo.somar(a, b);
+    }
+
+    private double Adiciona(double a, double b)
+    {
+        return calculo.somar(a, b);
+    }
 }
